926df7972d0b55efc272cea5d1f037f46972a5c6
Danish Siddiqui <danbot555@gmail.com>
2024-03-03 18:50:19 -0500

    setting up

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template("leaderboard.html")
+
+
+app.run(
+    #host="0.0.0.0", port=5000
+    debug = True, port = 3000
+        )
