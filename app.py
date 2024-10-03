c60c9a60cd230a2bb370a0d627cd1083a2cb4b0d
Danish Siddiqui <danbot555@gmail.com>
2024-10-03 18:48:30 -0400

    Added elo.py file

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import random
 import get_player_data as pd
+import elo
 
 
 from flask import Flask, render_template, request, jsonify
@@ -46,61 +47,6 @@ def update_player_elo_to_firebase(player_id, new_elo):
     firebase_db.child('data/players').child(str(player_id)).update({'ELO': new_elo})
     return True
 
-def get_tier_from_elo(player_elo: int):
-    tier = 0
-    if 0 <= player_elo < 300:
-            tier = 'Bronze'
-            print(f'{tier} tier achieved!')
-    elif 300 <= player_elo < 600:
-            tier = 'SilverI'
-            print(f'{tier} tier achieved!')
-    elif 600 <= player_elo < 900:
-            tier = 'SilverII'
-            print(f'{tier} tier achieved!')
-
-    elif 900 <= player_elo < 1200:
-            tier = 'SilverIII'
-            print(f'{tier} tier achieved!')
-
-    elif 1200 <= player_elo < 1500:
-            tier = 'GoldI'
-            print(f'{tier} tier achieved!')
-    elif 1500 <= player_elo < 1800:
-            tier = 'GoldII'
-            print(f'{tier} tier achieved!')
-    elif 1800 <= player_elo < 2100:
-            tier = 'Platinum'
-            print(f'{tier} tier achieved!')
-    elif 2100 <= player_elo < 2400:
-            tier = 'Diamond'
-            print(f'{tier} tier achieved!')
-    elif 2400 <= player_elo < 2700:
-            tier = 'Ruby'
-            print(f'{tier} tier achieved!')
-    elif 2700 <= player_elo < 3000:
-            tier = 'Obsidian'
-            print(f'{tier} tier achieved!')
-    elif player_elo >= 3000:
-            tier = 'Crystal'
-            print(f'{tier} tier achieved!')
-    return tier
-
-
-def calculate_elo(winning_elo, losing_elo):
-    winning_expected_score  = 1/(1+(10**((losing_elo-winning_elo)/400)))
-
-    k = 0
-    if winning_elo < 2100:
-        k = 32
-    elif 2100 < winning_elo < 2400:
-        k = 24
-    else:
-        k = 16
-    
-    new_elo_win = int(round(winning_elo + k*(1-winning_expected_score)))
-    new_elo_loss = int(round(losing_elo - k*(1-winning_expected_score)))
-    return new_elo_win, new_elo_loss
-
 '------------------'
 
     
@@ -135,13 +81,13 @@ def update_player():
     losing_data = get_data_from_firebase(str(losing_player))
 
     # Calculate new Elo ratings
-    new_elo_win, new_elo_loss = calculate_elo(winning_data['ELO'], losing_data['ELO'])
+    new_elo_win, new_elo_loss = elo.calculate_elo(winning_data['ELO'], losing_data['ELO'])
     print("winning elo: ", new_elo_win)
     print("losing elo: ", new_elo_loss)
 
     # Check if tier has changed
-    old_tier = get_tier_from_elo(winning_data['ELO'])
-    new_tier = get_tier_from_elo(new_elo_win)
+    old_tier = elo.from_elo(winning_data['ELO'])
+    new_tier = elo.get_tier_from_elo(new_elo_win)
 
     tier_changed = False
 
---
+++ elo.py
@@ -0,0 +1,56 @@
+# In this file, player elo's and tiers can be calculated. 
+
+def get_tier_from_elo(player_elo: int):
+    tier = 0
+    if 0 <= player_elo < 300:
+            tier = 'Bronze'
+            print(f'{tier} tier achieved!')
+    elif 300 <= player_elo < 600:
+            tier = 'SilverI'
+            print(f'{tier} tier achieved!')
+    elif 600 <= player_elo < 900:
+            tier = 'SilverII'
+            print(f'{tier} tier achieved!')
+
+    elif 900 <= player_elo < 1200:
+            tier = 'SilverIII'
+            print(f'{tier} tier achieved!')
+
+    elif 1200 <= player_elo < 1500:
+            tier = 'GoldI'
+            print(f'{tier} tier achieved!')
+    elif 1500 <= player_elo < 1800:
+            tier = 'GoldII'
+            print(f'{tier} tier achieved!')
+    elif 1800 <= player_elo < 2100:
+            tier = 'Platinum'
+            print(f'{tier} tier achieved!')
+    elif 2100 <= player_elo < 2400:
+            tier = 'Diamond'
+            print(f'{tier} tier achieved!')
+    elif 2400 <= player_elo < 2700:
+            tier = 'Ruby'
+            print(f'{tier} tier achieved!')
+    elif 2700 <= player_elo < 3000:
+            tier = 'Obsidian'
+            print(f'{tier} tier achieved!')
+    elif player_elo >= 3000:
+            tier = 'Crystal'
+            print(f'{tier} tier achieved!')
+    return tier
+
+
+def calculate_elo(winning_elo, losing_elo):
+    winning_expected_score  = 1/(1+(10**((losing_elo-winning_elo)/400)))
+
+    k = 0
+    if winning_elo < 2100:
+        k = 32
+    elif 2100 < winning_elo < 2400:
+        k = 24
+    else:
+        k = 16
+    
+    new_elo_win = int(round(winning_elo + k*(1-winning_expected_score)))
+    new_elo_loss = int(round(losing_elo - k*(1-winning_expected_score)))
+    return new_elo_win, new_elo_loss
